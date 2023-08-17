381e58eb1ab1501a1959f7db71f66d3467ddd0dc
Irfan <irfan199927@gmail.com>
2023-08-17 14:30:50 +0530

    push

--- Linked_List/remove_Nth_node_from_end.java
+++ Linked_List/remove_Nth_node_from_end.java
@@ -90,4 +90,20 @@ class Solution {
         pre.next = j.next;
         return head;
     }
+        //Best approach
+    //   public ListNode removeNthFromEnd(ListNode head, int n) {
+    //     ListNode fast = head, slow = head;
+    //     for(int i=0; i<n; i++){
+    //         fast = fast.next;
+    //     }
+    //     if(fast == null){
+    //         return head.next;
+    //     }
+    //     while(fast.next != null){
+    //         fast = fast.next;
+    //         slow = slow.next;
+    //     }
+    //     slow.next = slow.next.next;
+    //     return head;
+    // }
 }
