3689dd606450f21138d0239f240d01ed1f3a71b9
Irfan <irfan199927@gmail.com>
2023-09-19 21:10:36 +0530

    push

--- Stack/prefx.java
+++ Stack/prefx.java
@@ -1,27 +1,32 @@
+package Stack;
+
 import java.util.Stack;
 
 public class prefx {
     public static void main(String[] args) {
 
         String str = "9-(5+3)*4/6";
+        System.out.println(str);
 		Stack<String> val = new Stack<>();
 		Stack<Character> op = new Stack<>();
+
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
             int ascii = (int)(ch);
-            if(ascii >=48 && ascii <= 57){
-                String ans = ""+ch;
+            if(ascii >= 48 && ascii <= 57){
+                String ans = "" + ch;
                 val.push(ans);
             }
-            else if(op.empty() || ch == '(' || op.peek() == '(') op.push(ch);
+            else if(op.empty() || ch == '(' || op.peek() == '(') 
+                op.push(ch);
             
             else if(ch == ')'){
                 while(op.peek() != '('){
                     String val2 = val.pop();
                     String val1 = val.pop();
                     char o = op.pop();
-                    val.push(o+val1+val2);
-                    op.pop();
+                    String t = o+val1+val2;
+                    val.push(t);
                 }
                 op.pop(); // opening bracket Pop;
             }
@@ -32,7 +37,6 @@ public class prefx {
                     String val1 = val.pop();
                     char o = op.pop();
                     val.push(o+val1+val2);
-                    op.pop();
                     op.push(ch);
                 }
                 if(op.peek() =='*' || op.peek() == '/'){
@@ -40,7 +44,6 @@ public class prefx {
                     String val1 = val.pop();
                     char o = op.pop();
                     val.push(o+val1+val2);
-                    op.pop();
                     op.push(ch);
                 }
                 else op.push(ch); // this is to push Multiplication or Divison 
@@ -51,7 +54,6 @@ public class prefx {
             String val1 = val.pop();
             char o = op.pop();
             val.push(o+val1+val2);
-            op.pop();
         }
         System.out.println(val.peek());
     }
