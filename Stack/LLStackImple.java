4db66e4d957ef27c174fe0d498210b177504a760
Irfan <irfan199927@gmail.com>
2023-09-19 13:13:13 +0530

    push

---
+++ Stack/LLStackImple.java
@@ -0,0 +1,17 @@
+public class LLStackImple {
+    public static class Node{
+        int val;
+        int node;
+    }    
+    public static class LLStack{
+        Node head = null;
+        int size = 0;
+
+        void push(int x){
+            Node temp = new Node();
+            temp.next = head;
+            
+        }
+    }
+}
+
